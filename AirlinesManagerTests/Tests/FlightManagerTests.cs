4c673b72aeb34490ce25df7c1d12c5248b659d84
Jordan G <jorg2332@gmail.com>
2018-02-22 00:32:09 -0500

    calculates the distance between airports

--- AirlinesManagerTests/Tests/FlightManagerTests.cs
+++ AirlinesManagerTests/Tests/FlightManagerTests.cs
@@ -38,5 +38,20 @@ namespace AirlinesManagerTests
             SaltLakeCityAirport.Instance.LandPlane(plane);
             Assert.AreEqual(1, SaltLakeCityAirport.Instance.DockedAirplanes.Count);
         }
+
+        [TestMethod]
+        public void AirportNameToURLFormatTest()
+        {
+            var airportName = SaltLakeCityAirport.Instance.Name;
+            Assert.AreEqual("Salt+Lake+City+Airport", airportName.Replace(' ', '+'));
+        }
+
+        [TestMethod]
+        public void DistanceCalculator()
+        {
+            int distance = AirlinesManagerGame.FlightManager.GoogleGeoCodeRequest.GetDistance(SaltLakeCityAirport.Instance, PhoenixAirport.Instance);
+
+            Assert.AreEqual(508, distance);
+        }
     }
 }
