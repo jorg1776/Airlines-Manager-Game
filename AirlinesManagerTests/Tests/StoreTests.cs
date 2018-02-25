bf99d1069161f9211d201ac25e0d272295e71478
Jordan G <jorg2332@gmail.com>
2018-02-25 18:21:03 -0500

    added some airplane purchasing tests

---
+++ AirlinesManagerTests/Tests/StoreTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using AirlinesManagerGame.Models;
+using AirlinesManagerGame.ViewModels;
+using AirlinesManagerGame.Services.Mediators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirlinesManagerTests.Tests
+{
+    [TestClass]
+    public class StoreTests
+    {
+        [TestMethod]
+        public void StorePopulating_StoreNotEmpty()
+        {
+            //Arrange
+            var storeVM = new StoreViewModel();
+
+            //Act
+            var availableAirplanes = storeVM.AvailableAirplanesList;
+
+            //Assert
+            Assert.IsTrue(availableAirplanes.Count > 0);
+        }
+
+        [TestMethod]
+        public void PurchaseVerification_CorrectAirplaneVerified()
+        {
+            //Arrange
+            var purchaseVerificationVM = new PurchaseVerificationViewModel();
+            var airplane = new Bearclaw();
+
+            //Act
+            purchaseVerificationVM.ValidatePurchase(airplane);
+
+            //Assert
+            Assert.AreEqual((String.Format("Would you like to purchase {0} for {1}?", airplane.Name, airplane.PriceAsString)), purchaseVerificationVM.VerificationQuestion);
+        }
+
+        [TestMethod]
+        public void AddPurchasedAirplane_CorrectAirplaneAdded()
+        {
+            //Arrange
+            var mediator = new AirplanePurchaseMediator();
+            var airplanesStatusVM = new AirplanesStatusViewModel();
+            var airplane = new Bearclaw();
+
+            //Act
+            AirplanePurchaseMediator.AddAirplane(this,airplane);
+
+            //Assert
+            Assert.IsTrue(airplanesStatusVM.AirplanesList.Count > 0);
+            Assert.IsTrue(airplanesStatusVM.AirplanesList.Contains(airplane));
+        }
+    }
+}
