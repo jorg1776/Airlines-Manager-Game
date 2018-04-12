058807e723de5fdb6396e69acfbaeb9acfeba7c1
Jordan G <jorg2332@gmail.com>
2018-04-12 09:45:43 -0600

    Modified AirportStoreView to show available AND owned airports

--- ViewModels/StoreViewModels/AirportStoreViewModel.cs
+++ ViewModels/StoreViewModels/AirportStoreViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using AirlinesManagerGame.Models;
+using AirlinesManagerGame.Services.Mediators;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace AirlinesManagerGame.ViewModels
 {
@@ -7,8 +9,21 @@ namespace AirlinesManagerGame.ViewModels
     {
         public ObservableCollection<Airport> AirportsList { get { return store.Airports; } }
 
+        public ObservableCollection<Airport> OwnedAirports { get { return user.OwnedAirports; } }
+
         public AirportStoreViewModel(User _user) : base(_user)
         {
+            ItemPurchaseMediator.OnAirportPurchased += AddAirport;
+        }
+
+        private void AddAirport(object sender, ItemPurchaseMediator.AirportPurchasedEventArgs e)
+        {
+            var purchasedAirport = e.PurchasedAirport;
+            OwnedAirports.Add(purchasedAirport);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, purchasedAirport));
+
+            AirportsList.Remove(purchasedAirport);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, purchasedAirport));
         }
     }
 }
